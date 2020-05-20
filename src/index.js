457012b15cb89b2d50f3135a2be639d19652bf66
Teemu <teemu.salminen96@gmail.com>
2020-05-20 07:14:16 +0300

    Fix cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'https://tranquil-badlands-12355.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
